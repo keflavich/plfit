1eaadc6caa99f07913df5ef01e1bd7b38f2f42f8
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-12-12 11:53:47 +0100

    relative imports

--- plfit/__init__.py
+++ plfit/__init__.py
@@ -1,4 +1,4 @@
 #from plfit import discrete_likelihood
-from plfit import plfit
-from plfit import test_fitter,pl_inv,plexp_inv,plexp_cdf, plexp_pdf, plfit_lsq
-from plfit_py import plfit as plfit_py
+from .plfit import plfit
+from .plfit import test_fitter,pl_inv,plexp_inv,plexp_cdf, plexp_pdf, plfit_lsq
+from .plfit_py import plfit as plfit_py
